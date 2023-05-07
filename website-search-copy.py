2cb8d8651a7c5298e1b1d6cbab2abc872a40b5d7
Christine O'Connor <coconn20@jh.edu>
2023-05-07 15:30:28 -0400

    print info

--- website-search-copy.py
+++ website-search-copy.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+
+
 
 # Set up the WebDriver
 driver = webdriver.Chrome()
@@ -21,24 +24,42 @@ search_bar.send_keys(Keys.RETURN)
 # search_button.click()
 
 # Wait for the search results to load
-wait = WebDriverWait(driver, 30)
+#wait = WebDriverWait(driver, 10)
 #wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "placardBanner")))
+import time
+time.sleep(10)
 
 # Print out the names of the apartments
 listings = driver.find_elements(By.CLASS_NAME, "property-info")
+
 for ele in listings:
-    #print(name.text)
-    #name = ele.get_attribute("aria-label")
-    #link = ele.get_attribute("href")
-    #print(name, ":", link)
-    listing = listings.find_element(By.CLASS_NAME, "property-link")
-    name = listing.get_attribute("aria-label")
-    link = listing.get_attribute("href")
+    try:
+        listing = ele.find_element(By.CLASS_NAME, "property-link")
+        name = listing.get_attribute("aria-label")
+        link = listing.get_attribute("href")
+        try:
+            price = ele.find_element(By.CLASS_NAME, "property-pricing").text
+        except NoSuchElementException:
+            try:
+                price = ele.find_element(By.CLASS_NAME, "property-rents").text
+            except:
+                price = ele.find_element(By.CLASS_NAME, "price-range").text
+        print(name, "|", link, "|", price)
+    except StaleElementReferenceException:
+        # Retry the element lookup in case of StaleElementReferenceException
+        listing = ele.find_element(By.CLASS_NAME, "property-link")
+        name = listing.get_attribute("aria-label")
+        link = listing.get_attribute("href")
+        try:
+            price = ele.find_element(By.CLASS_NAME, "property-pricing").text
+        except NoSuchElementException:
+            try:
+                price = ele.find_element(By.CLASS_NAME, "property-rents").text
+            except:
+                price = ele.find_element(By.CLASS_NAME, "price-range").text
+        print(name, "|", link, "|", price)
+    
 
-
-apartment_prices = driver.find_elements(By.CLASS_NAME, "property-pricing")
-for ele in apartment_prices:
-    print(ele.text)
 
 print("done")
 
