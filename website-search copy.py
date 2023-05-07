211946c54552914c786ae2efe4277dca4e70673c
Christine O'Connor <coconn20@jh.edu>
2023-05-07 14:51:52 -0400

    extracting listings

---
+++ website-search copy.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+# Set up the WebDriver
+driver = webdriver.Chrome()
+
+# Navigate to the apartments.com website
+driver.get("https://www.apartments.com/search/")
+
+# Find the search bar and enter the location you want to search for
+search_bar = driver.find_element(By.ID, "searchBarLookup")
+search_bar.send_keys("charles village")
+search_bar.send_keys(Keys.RETURN)
+
+
+# # Find the search button and click it
+# search_button = driver.find_element(By.Title, "search-submit-button")
+# search_button.click()
+
+# Wait for the search results to load
+wait = WebDriverWait(driver, 30)
+#wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "placardBanner")))
+
+# Print out the names of the apartments
+listings = driver.find_elements(By.CLASS_NAME, "property-link")
+for ele in listings:
+    #print(name.text)
+    name = ele.get_attribute("aria-label")
+    link = ele.get_attribute("href")
+    print(name, ":", link)
+
+
+apartment_prices = driver.find_elements(By.CLASS_NAME, "property-pricing")
+for ele in apartment_prices:
+    print(ele.text)
+
+print("dont")
+
+# Close the browser
+input()
+driver.quit()
